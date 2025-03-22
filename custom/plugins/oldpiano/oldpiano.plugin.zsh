cea653dc1082e1478d8919a6c302d1546d71ff9e
oldpiano <oldpiano@gmail.com>
2025-03-22 22:24:06 +0900

    hstr zshrc updated

--- custom/plugins/oldpiano/oldpiano.plugin.zsh
+++ custom/plugins/oldpiano/oldpiano.plugin.zsh
@@ -49,4 +49,13 @@ export DISABLE_UPDATE_PROMPT=true
 alias hh=hstr                    # hh to be alias for hstr
 setopt histignorespace           # skip cmds w/ leading space from history
 export HSTR_CONFIG=hicolor,prompt-bottom,raw-history-view       # get more colors
-bindkey -s "\C-r" "\C-a hstr -- \C-j"     # bind hstr to Ctrl-r (for Vi mode check doc)
+hstr_no_tiocsti() {
+    zle -I
+    { HSTR_OUT="$( { </dev/tty hstr ${BUFFER}; } 2>&1 1>&3 3>&- )"; } 3>&1;
+    BUFFER="${HSTR_OUT}"
+    CURSOR=${#BUFFER}
+    zle redisplay
+}
+zle -N hstr_no_tiocsti
+bindkey '\C-r' hstr_no_tiocsti
+export HSTR_TIOCSTI=n
